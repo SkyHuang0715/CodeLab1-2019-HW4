d8c720e37e1ad6782660e0b1bd961fbc2379c582
SkyHuang0715 <she515996344@gmail.com>
2019-02-25 14:58:13 -0500

    in class scripts

--- CodeLab-2019-HW4/Assets/Scripts/GameManager.cs
+++ CodeLab-2019-HW4/Assets/Scripts/GameManager.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    int score;
+    //int score;
+    public Text scoreText;
+    public Text highScoreText;
+
+    private const string PLAYER_PREF_HIGHSCORE = "highScore "; //const= constant means sth doest change
+
+    int score = 0;
 
     public int Score
     {
@@ -16,8 +23,29 @@ public class GameManager : MonoBehaviour
         
         set
         {
-            score = value; 
+            score = value;
+            scoreText.text = "Score " + score;
+            HighScore = score;
+        }
+    }
+
+    private int highScore = 0;
+
+    public int HighScore
+    {
+        get
+        {
+            return highScore;
             
+        }
+        set
+        {
+            if (value > highScore) //when score > high score, renew the high score to current score
+            {
+                highScore = value;
+                highScoreText.text = "High Score " + highScore;
+                PlayerPrefs.SetInt(PLAYER_PREF_HIGHSCORE, highScore);
+            }
         }
     }
 
@@ -35,7 +63,15 @@ public class GameManager : MonoBehaviour
             
 
         }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        HighScore = PlayerPrefs.GetInt(PLAYER_PREF_HIGHSCORE, 10);
+        
     }
+    
 
     // Update is called once per frame
     void Update()
