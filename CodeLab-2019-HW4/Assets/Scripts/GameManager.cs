ea1d5e8fae3d7385676eac49989338397fdffa9f
SkyHuang0715 <she515996344@gmail.com>
2019-02-25 14:12:13 -0500

    project create

---
+++ CodeLab-2019-HW4/Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int score;
+
+    public int Score
+    {
+
+        get
+        {
+            return score;
+        }
+        
+        set
+        {
+            score = value; 
+            
+        }
+    }
+
+    public static GameManager instance;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
